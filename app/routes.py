b16b23e344605878dc66b969af30b59832997d4c
deadlyparkurkiller <kartieltv@gmail.com>
2022-02-28 18:59:46 +0200

    Add Db

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template
-from forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,7 +11,10 @@ def index():
     return render_template("index.html", title="Home", user=user)
 
 
-@app.route("/login")
+@app.route("/login", methods={"GET", "POST"})
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"login requested for {form.username.data}, remember me ={form.remember_me.data}")
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign in", form=form)
