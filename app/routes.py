a78ab35dd82656f194f6665fec369f34f7153598
deadlyparkurkiller <kartieltv@gmail.com>
2022-03-01 19:09:34 +0200

    abb profike

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,89 @@
 # -*- coding: utf-8 -*-
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, ReqisterForm, EditProfileForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Jack"}
-    return render_template("index.html", title="Home", user=user)
+    return render_template("index.html", title='Home Page')
 
 
 @app.route("/login", methods={"GET", "POST"})
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"login requested for {form.username.data}, remember me ={form.remember_me.data}")
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid password ob Login")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign in", form=form)
+
+
+@app.route("/logut")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = ReqisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now registered user!")
+        return redirect(url_for("index"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "test post1",},
+        {"author": user, "body": "test post2",}
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
